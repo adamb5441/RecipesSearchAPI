f8b74150531af82efe1ab1f5e34acf3933c11662
adam.brown <adam.brown@JBRL023>
2021-03-02 17:07:50 -0600

    init query  setup

---
+++ src/RecipesAPI.Application/Recipes/GetFullRecipe/GetFullRecipeQuery.cs
@@ -0,0 +1,19 @@
+﻿using RecipesAPI.Application.Configuration.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesAPI.Application.Recipes.GetFullRecipe
+{
+    public class GetFullRecipeQuery : IQuery<RecipeDto>
+    {
+        public GetFullRecipeQuery(Guid recipeId)
+        {
+            RecipeId = recipeId;
+        }
+
+        public Guid RecipeId { get; }
+    }
+}
